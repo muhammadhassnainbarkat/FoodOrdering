8e447b8b6166495a76d76a56bd6043a67ef77567
Muhammad Hassnain Barkat <muhammadhassnainbarkat@gmail.com>
2024-08-21 04:51:28 +0500

    setup cart

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,7 +3,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/components/useColorScheme';
@@ -52,7 +52,7 @@ function RootLayoutNav() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
       </Stack>
     </ThemeProvider>
   );
---
+++ src/app/cart.tsx
@@ -0,0 +1,13 @@
+import {View, Text, Platform} from "react-native";
+import {StatusBar} from "expo-status-bar";
+
+const CartScreen = () => {
+  return (
+      <View>
+          <Text>Cart Screen</Text>
+          <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      </View>
+  );
+};
+
+export default CartScreen;
